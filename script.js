c279d099cd2d40bc1814d8b408f7c117674c08cc
utkarsh736 <utkarshtomar736@gmail.com>
2022-03-05 13:14:01 +0530

    Added book constuctor

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let myLibrary = [];
 
-function Book(){
-    // The constructor
-}
+function Book(title, author, pages, isRead=false){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+    this.info = function(){
+        if (isRead == true){
+            return(`The ${title} by ${author}, ${pages} pages, already read.`);
+        }
+        else if(isRead == false){
+            return(`The ${title} by ${author}, ${pages} pages, not read yet.`);
+        };
+    };
+};
+
+let book1 = new Book('Harry Potter', 'J.K. Rowling', 295, true);
+console.log(book1.info());
+
 
 function addBookToLibrary(){
     // Function
