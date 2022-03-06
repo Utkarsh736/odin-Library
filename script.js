9630c659cecc2d1747a53e5b53594cc4e35f9c61
utkarsh736 <utkarshtomar736@gmail.com>
2022-03-06 10:11:41 +0530

    [Added] book list

--- script.js
+++ script.js
@@ -17,7 +17,10 @@ function Book(title, author, pages, isRead=false){
 };
 
 let book1 = new Book('Harry Potter', 'J.K. Rowling', 295, true);
-console.log(book1.info());
+let book2 = new Book('Sherlock Holmes', 'Arthur Conan Doyle', 500);
+let book3 = new Book('Game of Thrones', 'George RR Martin', 300, true);
+let book4 = new Book('The Shining', 'Stephen King', 400);
+let book5 = new Book('Mrs. Flowers', 'Maya Angelou', 250, true);
 
 
 function addBookToLibrary(){
