f0dc744c98fab2c279a2bce8682998a7a2144653
utkarsh736 <utkarshtomar736@gmail.com>
2022-03-06 14:52:39 +0530

    [Added] function to display books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector('.display');
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead=false){
@@ -5,24 +7,40 @@ function Book(title, author, pages, isRead=false){
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-
-    this.info = function(){
-        if (isRead == true){
-            return(`The ${title} by ${author}, ${pages} pages, already read.`);
-        }
-        else if(isRead == false){
-            return(`The ${title} by ${author}, ${pages} pages, not read yet.`);
-        };
-    };
+    
 };
+
+Book.prototype.info = function(isRead){
+    if (this.isRead == true){
+        return(`The ${this.title} by ${this.author}, ${this.pages} pages, already read.`);
+    }
+    else if(this.isRead == false){
+        return(`The ${this.title} by ${this.author}, ${this.pages} pages, not read yet.`);
+    };
+}; 
 
 let book1 = new Book('Harry Potter', 'J.K. Rowling', 295, true);
 let book2 = new Book('Sherlock Holmes', 'Arthur Conan Doyle', 500);
 let book3 = new Book('Game of Thrones', 'George RR Martin', 300, true);
-let book4 = new Book('The Shining', 'Stephen King', 400);
+let book4 = new Book('Shining', 'Stephen King', 400);
 let book5 = new Book('Mrs. Flowers', 'Maya Angelou', 250, true);
 
 
+
 function addBookToLibrary(){
-    // Function
+    myLibrary.push(book1, book2, book3, book4, book5);
 }
+
+function main(){
+
+    for(let i=0; i<myLibrary.length; i++){
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.append(myLibrary[i].title);
+        console.log(card);
+        display.appendChild(card);
+    };
+};
+
+addBookToLibrary();
+main();
