e39b1401296b4563621f58f6da40e3426f92d852
utkarsh736 <utkarshtomar736@gmail.com>
2022-03-16 13:19:16 +0530

    Add read status

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const display = document.querySelector('.display');
 const modalContainer = document.querySelector('#modal-container');
 const form = document.querySelector('#form');
+// const status = document.querySelector('#status');
 const addBook = document.querySelector('#add-book');
 const submit = document.querySelector('#submit');
-
 
 let myLibrary = [];
 
@@ -15,13 +15,17 @@ function Book(title, author, pages, isRead=false){
     
 };
 
-Book.prototype.info = function(isRead){
+Book.prototype.readStatus = function(isRead){
+    let status = 'Not Read';
+
     if (this.isRead == true){
-        return(`The ${this.title} by ${this.author}, ${this.pages} pages, already read.`);
+       status.innerHtml = 'Read';
     }
     else if(this.isRead == false){
-        return(`The ${this.title} by ${this.author}, ${this.pages} pages, not read yet.`);
+        status.innerHtml = 'Not Read';
     };
+
+    return status;
 }; 
 
 let book1 = new Book('Harry Potter', 'J.K. Rowling', 295, true);
@@ -36,6 +40,8 @@ function myFunction() {
     let formTitle = form.title.value;
     let formAuthor = form.author.value;
     let formPages = form.pages.value;
+    console.log(Book.readStatus);
+
 
     bookNew = new Book(formTitle, formAuthor, formPages);
     addBookToLibrary(bookNew);
@@ -58,19 +64,23 @@ function main(){
     const bookTitle = document.createElement('p');
     const bookAuthor = document.createElement('p');
     const bookPages = document.createElement('p');
+    const bookReadStatus = document.createElement('button');
 
     card.classList.add('card');
     bookTitle.classList.add('title');
     bookAuthor.classList.add('author');
     bookPages.classList.add('pages');
+    bookReadStatus.classList.add('readStatus');
 
     bookTitle.append(myLibrary[i].title);
     bookAuthor.append(myLibrary[i].author);
     bookPages.append(myLibrary[i].pages);
+    bookReadStatus.append(myLibrary[i].readStatus());
 
     card.appendChild(bookTitle);
     card.appendChild(bookAuthor);
     card.appendChild(bookPages);
+    card.appendChild(bookReadStatus);
 
     display.appendChild(card);
 };
